96089594c22f9d856b9cc1551042748b8c6d4281
Bernidelpino <b.delpinovillaseca@gmail.com>
2023-06-22 22:45:58 -0400

    prueba nocturna

--- src/main.js
+++ src/main.js
@@ -1,14 +1,43 @@
-//import { funciones } from './data.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.json';
 
 
+//import { example } from './data.js';
+// import data from './data/lol/lol.js';
+import data from './data/pokemon/pokemon.json' assert {type:'json'} ;
 
+//hacer arreglo en una constante.
+const ArrayDePokemons = data.pokemon;
 
+//obtener el botón para empezar
 const button = document.getElementById("comenzar");
 button.addEventListener("click", function () {
-  window.location.href= 'index2.html';
+  window.location.href= 'index.html';
 });
-
-console.log("example", data);
-
-
+//declara una lista, la encuentra por su id.
+const viewPokemonList = document.getElementById("root");
+//Crea una estructura de repetición con el arreglo que guardo en la linea 9.
+for(const i of ArrayDePokemons){
+  //Crea un elemento div que se llamará cardDataPokemon.
+  const cardDataPokemon = document.createElement('div');
+  //Le entrega una clase que se llama VistaDeTarjetas.
+  cardDataPokemon.setAttribute('class','VistaDeTarjetas');
+  //crea elementos , y una imagen dentro
+  //####################################################
+  const na = document.createElement('p');
+  const imgn = document.createElement('img');
+  const num = document.createElement('p');
+  const desc = document.createElement('p');
+  //####################################################
+  //Declara variable imagen (imgn) buscando en el arreglo con un contador y su propiedad img.
+  imgn.src = i.img;
+  //Lo mismo pero con textcontent
+  num.textContent = i.num;
+  desc.textContent = i.about;
+  na.textContent = i.name;
+  //AppendChild surve para poner un valor o imagen en el html.
+  cardDataPokemon.appendChild(na);
+  cardDataPokemon.appendChild(imgn);
+  cardDataPokemon.appendChild(num);
+  cardDataPokemon.appendChild(desc);
+  viewPokemonList.appendChild(cardDataPokemon);
+}
